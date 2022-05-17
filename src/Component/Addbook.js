7fbaa9c3b9db88a889c56075b86e822a48cd2e56
jyothymol999 <jyothymol12@gmail.com>
2022-05-17 02:02:47 -0700

    navbar

--- src/Component/Addbook.js
+++ src/Component/Addbook.js
@@ -1,26 +1,28 @@
 import React from 'react'
+import Header from './Header'
 
 const Addbook = () => {
   return (
     <div>
-        <div class="container">
-    <div class="row">
-        <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-            <div class="row g-3">
-        <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-            <label for="" class="form-label">Booktitle</label>
-            <input type="text" class="form-control"/>     
+        <Header/>
+        <div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <label for="" className="form-label">Booktitle</label>
+            <input type="text" className="form-control"/>     
         </div>
-        <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-            <label for="" class="form-control">Author</label>
-            <input type="text" class="form-control"/>  
+        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+            <label for="" className="form-label">Author</label>
+            <input type="text" className="form-control"/>  
         </div>
-        <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-            <label for="" class="form-control">price</label>
-            <input type="text" class="form-control"/>  
+        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+            <label for="" className="form-label">price</label>
+            <input type="text" className="form-control"/>  
         </div>
-        <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-            <button class="btn-success">SUBMIT</button> 
+        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+            <button className="btn-success">SUBMIT</button> 
         </div>
 
     </div>
--- src/Component/Searchbook.js
+++ src/Component/Searchbook.js
@@ -1,18 +1,20 @@
 import React from 'react'
+import Header from './Header'
 
 const Searchbook = () => {
   return (
     <div>
-      <div class="container">
-    <div class="row">
-        <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-            <div class="row g-3">
-        <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-            <label for="" class="form-label">Booktitle</label>
-            <input type="text" class="form-control"/>     
+        <Header/>
+      <div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <label for="" className="form-label">Booktitle</label>
+            <input type="text" className="form-control"/>     
         </div>
-        <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-            <button class="btn-success">SEARCH</button> 
+        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+            <button className="btn-success">SEARCH</button> 
         </div>
     </div>
     </div>
